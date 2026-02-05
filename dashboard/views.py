59c1d35dc79fcfc02d035b17329df7d752c72a61
Nikitadevp <jrdme.rbpl@rajat-group.com>
2026-02-05 10:56:06 +0530

    Initial commit

---
+++ dashboard/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+def home(request):
+
+    tickets = [
+        {"no":"TCKT-486B8036","name":"vijay","department":"JNRDME","priority":"Normal","status":"Closed","date":"30 Jan 2026"},
+        {"no":"TCKT-D70E2285","name":"namita","department":"JNRDME","priority":"Urgent","status":"Closed","date":"29 Jan 2026"},
+    ]
+
+    context = {
+        "total": 28,
+        "open": 16,
+        "closed": 6,
+        "urgent": 22,
+        "tickets": tickets
+    }
+
+    return render(request, "dashboard.html", context)
