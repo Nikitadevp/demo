f898b9d84c425180956183bc8e324fe4cf32f45b
Nikitadevp <jrdme.rbpl@rajat-group.com>
2026-02-27 17:13:44 +0530

    fixed gunicorn

--- demo/settings.py
+++ demo/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'demo.urls'
@@ -119,5 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   # 👈 agar tumhara static folder root me hai
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
