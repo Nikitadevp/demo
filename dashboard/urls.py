54657ea8eb3c33504115215d936973c65f4547c3
Nikitadevp <jrdme.rbpl@rajat-group.com>
2026-02-28 17:10:08 +0530

    set ticket form as home

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,7 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    #path('', views.dashboard, name='dashboard'),
+    path("", views.ticket_form, name="home"),   # 👈 Ye line change karo
+    path("dashboard/", views.dashboard, name="dashboard"),
     path("raise-ticket/", views.raise_ticket, name="raise_ticket"),
    
 ]
