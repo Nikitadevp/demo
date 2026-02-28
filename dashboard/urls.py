5b25fb8b8e184a8fcf313d0721cfb42def151d7e
Nikitadevp <jrdme.rbpl@rajat-group.com>
2026-02-28 17:55:46 +0530

    Fixed duplicate url and removed ticket_form error

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,8 +5,10 @@ from . import views
 
 urlpatterns = [
     #path('', views.dashboard, name='dashboard'),
-    path("", views.ticket_form, name="home"),   # 👈 Ye line change karo
-    path("dashboard/", views.dashboard, name="dashboard"),
+   # path("", views.ticket_form, name="home"),   # 👈 Ye line change karo
+    #path("dashboard/", views.dashboard, name="dashboard"),
+    #path("raise-ticket/", views.raise_ticket, name="raise_ticket"),
+    #path("", views.dashboard, name="home"),
+    path("", views.dashboard, name="home"),   # homepage
     path("raise-ticket/", views.raise_ticket, name="raise_ticket"),
-   
 ]
