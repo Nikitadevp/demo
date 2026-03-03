438a4e0636a112646815638f66da2652ab6bb4c1
Nikitadevp <jrdme.rbpl@rajat-group.com>
2026-03-03 15:39:03 +0530

    fixed resolve ticket page

--- demo/settings.py
+++ demo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 
@@ -96,11 +97,15 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# } 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
