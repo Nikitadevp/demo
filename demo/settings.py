b1f40ca2641905b30f9d3f03ffc6637d4c1b26e2
Nikitadevp <jrdme.rbpl@rajat-group.com>
2026-03-13 15:02:13 +0530

    debug raise ticket

--- demo/settings.py
+++ demo/settings.py
@@ -128,11 +128,10 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 if "DATABASE_URL" in os.environ:
 
     DATABASES = {
-        "default": dj_database_url.parse(
-        "postgresql://demo_db_zfo0_user:oscZumsLVu9aMq3GrzFmJnIBcgfBUbKI@dpg-d6jan41r0fns73bkesc0-a.singapore-postgres.render.com:5432/demo_db_zfo0",
-        conn_max_age=600,
-        ssl_require=True
-      )
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
 
 else:
