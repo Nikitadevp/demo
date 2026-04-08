73d7d2f6393b3f26844ed5482faf39a4b99480ab
Nikitadevp <jrdme.rbpl@rajat-group.com>
2026-04-08 13:45:18 +0530

    fixed leave backend fields

--- dashboard/models.py
+++ dashboard/models.py
@@ -53,68 +53,7 @@ class Ticket(models.Model):
 
     def __str__(self):
         return self.ticket_no
-# from django.db import models
-# import uuid
-# from django.contrib.auth.models import User
 
-
-# def generate_ticket_no():
-#     return "TCKT-" + uuid.uuid4().hex[:8].upper()
-
-
-# class Ticket(models.Model):
-
-#     STATUS_CHOICES = [
-#         ("Open", "Open"),
-#         ("In Progress", "In Progress"),
-#         ("Resolved", "Resolved"),
-#         ("Closed", "Closed"),
-#     ]
-
-#     PRIORITY_CHOICES = [
-#         ("Urgent", "Urgent"),
-#         ("Normal", "Normal"),
-#     ]
-
-#     ticket_no = models.CharField(
-#         max_length=20,
-#         unique=True,
-#         default=generate_ticket_no,
-#         editable=False
-#     )
-
-#     name = models.CharField(max_length=100)
-#     email = models.EmailField()
-#     phone = models.CharField(max_length=20)
-#     department = models.CharField(max_length=100)
-#     issue_type = models.CharField(max_length=200)
-
-#     priority = models.CharField(
-#         max_length=20,
-#         choices=PRIORITY_CHOICES
-#     )
-
-#     status = models.CharField(
-#         max_length=20,
-#         choices=STATUS_CHOICES,
-#         default="Open"
-#     )
-
-#     tat_deadline = models.DateTimeField(null=True, blank=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-
-#     problem_solver = models.ForeignKey(
-#         User,
-#         on_delete=models.SET_NULL,
-#         null=True,
-#         blank=True
-#     )
-
-#     solution_provided = models.TextField(null=True, blank=True)
-#     resolved_at = models.DateTimeField(null=True, blank=True)
-
-#     def __str__(self):
-#         return self.ticket_no
 
 
 class LeaveRequest(models.Model):
@@ -149,3 +88,6 @@ class LeaveRequest(models.Model):
         if not self.leave_id:
             self.leave_id = f"LR-{uuid.uuid4().hex[:8].upper()}"
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.leave_id} - {self.name}"
