2859e527ddd1a6f2e21d87209c92c888ea3e0d1b
Nikitadevp <jrdme.rbpl@rajat-group.com>
2026-05-25 12:37:04 +0530

    fix media serving

--- demo/urls.py
+++ demo/urls.py
@@ -21,6 +21,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.urls import re_path
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -32,3 +35,12 @@ urlpatterns += static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT
 )
+
+
+urlpatterns += [
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT}
+    ),
+]
