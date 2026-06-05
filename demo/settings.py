7c315be9799b5c5ce165f13ebd96f9a33170ce9e
Nikitadevp <nikitadevp@gmail.com>
2026-06-05 11:15:45 +0530

    update from new setup

--- demo/settings.py
+++ demo/settings.py
@@ -24,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+#SECRET_KEY = 'local-secret-key-123'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['.onrender.com']
 
 # ALLOWED_HOSTS = ['demo-project-gkaa.onrender.com']
