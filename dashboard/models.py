f001dc48ca114a23117566ccf297476dead9af77
Nikitadevp <nikitadevp@gmail.com>
2026-06-23 11:08:00 +0530

    update from new setup

--- dashboard/models.py
+++ dashboard/models.py
@@ -597,3 +597,88 @@ class EstimateForm(models.Model):
     def __str__(self):
 
         return self.unique_id
+    
+
+
+
+#CustomerApproval
+
+class CustomerApproval(models.Model):
+
+    APPROVAL_CHOICES = [
+        ('Approve', 'Approve'),
+        ('Reject', 'Reject'),
+    ]
+
+    unique_id = models.CharField(
+        max_length=20,
+        unique=True,
+        blank=True
+    )
+
+    customer_query = models.OneToOneField(
+        CustomerQuery,
+        on_delete=models.CASCADE
+    )
+
+    email = models.EmailField()
+
+    uid = models.IntegerField()
+
+    customer_name = models.CharField(
+        max_length=100
+    )
+
+    block = models.CharField(
+        max_length=100
+    )
+
+    area = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+    case_id = models.CharField(
+        max_length=50
+    )
+
+    approval_type = models.CharField(
+        max_length=20,
+        choices=APPROVAL_CHOICES
+    )
+
+    customer_remark = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def generate_unique_id(self):
+
+        while True:
+
+            code = "CA" + ''.join(
+                random.choices(
+                    string.ascii_letters + string.digits,
+                    k=10
+                )
+            )
+
+            if not CustomerApproval.objects.filter(
+                unique_id=code
+            ).exists():
+
+                return code
+
+    def save(self, *args, **kwargs):
+
+        if not self.unique_id:
+
+            self.unique_id = self.generate_unique_id()
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+
+        return self.unique_id
