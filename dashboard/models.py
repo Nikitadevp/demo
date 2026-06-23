a256c9ee6ac4ebb2c39a4630adfbc2f2ecec3ad9
Nikitadevp <nikitadevp@gmail.com>
2026-06-23 17:48:25 +0530

    update from new setup

--- dashboard/models.py
+++ dashboard/models.py
@@ -761,3 +761,66 @@ class AdvanceCollection(models.Model):
     def __str__(self):
 
         return self.unique_id
+    
+
+
+
+class MaterialAvailability(models.Model):
+
+    unique_id = models.CharField(max_length=20, unique=True, blank=True)
+
+    customer_query = models.OneToOneField(
+        CustomerQuery,
+        on_delete=models.CASCADE
+    )
+
+    email = models.EmailField()
+
+    uid = models.IntegerField()
+
+    customer_name = models.CharField(max_length=100)
+
+    block = models.CharField(max_length=100)
+
+    area = models.CharField(max_length=100)
+
+    case_id = models.CharField(max_length=50)
+
+    material_available = models.CharField(
+        max_length=10,
+        choices=[
+            ('Yes', 'Yes'),
+            ('No', 'No')
+        ]
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def generate_unique_id(self):
+
+        while True:
+
+            code = "MA" + ''.join(
+                random.choices(
+                    string.ascii_letters + string.digits,
+                    k=10
+                )
+            )
+
+            if not MaterialAvailability.objects.filter(
+                unique_id=code
+            ).exists():
+
+                return code
+
+    def save(self, *args, **kwargs):
+
+        if not self.unique_id:
+
+            self.unique_id = self.generate_unique_id()
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+
+        return self.unique_id
