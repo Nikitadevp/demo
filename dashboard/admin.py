c5f1a6484ad8f67c9892f97e79ed787a530cb868
Nikitadevp <nikitadevp@gmail.com>
2026-06-30 16:07:04 +0530

    Added login JS

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,32 +1,7 @@
 from django.contrib import admin
-from .models import Ticket, MasterDashboard
+from .models import Ticket
 
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
     list_display = ("ticket_no", "name", "department", "priority", "status", "created_at")
-    
-
-@admin.register(MasterDashboard)
-class MasterDashboardAdmin(admin.ModelAdmin):
-
-    list_display = (
-        "ticket_id",
-        "customer_name",
-        "current_stage",
-        "current_status",
-        "priority",
-        "created_at"
-    )
-
-    list_filter = (
-        "current_stage",
-        "current_status",
-        "priority"
-    )
-
-    search_fields = (
-        "ticket_id",
-        "customer_name",
-        "contact"
-    )
     
