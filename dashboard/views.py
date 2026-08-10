b0126debea4dda38233f41a4f735f52e8f9baff1
Nikitadevp <nikitadevp@gmail.com>
2026-08-10 18:15:22 +0530

    Update CRM Dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -4331,7 +4331,7 @@ def crm_dashboard(request):
 
     total_queries = queries.count()
 
-    open_count = 0
+  
     
     in_progress_count = 0
    
@@ -4753,7 +4753,7 @@ def crm_dashboard(request):
 
         # Dashboard Counts
         "total_queries": total_queries,
-        "open_count": open_count,
+       
         
         "in_progress_count": in_progress_count,
         
