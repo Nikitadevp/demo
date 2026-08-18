9efdc7d521360d45c7ea6d362ce8032bd7b89fd9
Nikitadevp <nikitadevp@gmail.com>
2026-08-18 14:58:06 +0530

    Update CRM Dashboard

--- demo/urls.py
+++ demo/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 
 from django.views.static import serve
 from django.urls import re_path
+from django.conf.urls.static import static
 
 
 urlpatterns = [
