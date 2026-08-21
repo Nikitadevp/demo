2468d0537245f62523758661f725c7bb3d045498
Nikitadevp <nikitadevp@gmail.com>
2026-08-21 12:23:07 +0530

    Update CRM Dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -4426,7 +4426,16 @@ def crm_dashboard(request):
     if issue_filter:
         queries = queries.filter(issue=issue_filter)
     if customer_name_filter:
-       queries = queries.filter(name__icontains=customer_name_filter)
+       queries = queries.filter(name=customer_name_filter)
+
+    customer_name_options = sorted(
+        set(
+            CustomerQuery.objects
+            .values_list("name", flat=True)
+            .exclude(name="")
+        )
+    )
+
 
 
     tower_options = sorted(
@@ -4930,6 +4939,8 @@ def crm_dashboard(request):
             "closed_date": closed_date,
 
             "customer_name_filter": customer_name_filter,
+
+            "customer_name_options": customer_name_options,
 
             
 
